c39534845f398de617778ed4e4e8a4b5b8bc0c5b
BrianLee <brianleelxt@gmail.com>
2018-07-30 23:16:22 +0800

    add 66

---
+++ 66.Plus One/solution_1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i --) {
+            if (digits[i] != 9) {
+                digits[i] ++;
+                break;
+            }
+            else {
+                digits[i] = 0;
+            }
+        }
+        if (digits[0] == 0) {
+            int result[] = new int[len + 1];
+            result[0] = 1;
+            return result;
+        }
+        return digits;
+    }
+}
