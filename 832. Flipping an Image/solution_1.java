209fd61afbc6167ac9993fc93bea6407b6403839
BrianLee <brianleelxt@gmail.com>
2018-07-31 20:22:41 +0800

    add 832

---
+++ 832. Flipping an Image/solution_1.java
@@ -0,0 +1,16 @@
+//100%
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length / 2; j++) {
+                int temp = A[i][j];
+                A[i][j] = 1 - A[i][A[i].length - j - 1];
+                A[i][A[i].length - j - 1] = 1 - temp;
+            }
+            if (A[i].length % 2 == 1) {
+                A[i][A[i].length / 2] = 1- A[i][A[i].length / 2];
+            }
+        }
+        return A;
+    }
+}
