742f0de20bfec172599cc784d4bda19f868d0102
XutongLi <brianleelxt@gmail.com>
2019-09-16 20:45:36 +0800

    268 cpp

---
+++ 268. Missing Number/solution_1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int len = nums.size();
+        int sum = 0;
+        for (int i = 0; i < len; i ++) 
+            sum += nums[i];
+        return (1 + len) * len / 2 - sum;
+    }
+};
