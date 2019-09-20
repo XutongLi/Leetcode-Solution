8305c23204f2e5c0799e4e1474c7a5372e8e5d03
XutongLi <brianleelxt@gmail.com>
2019-09-20 21:40:54 +0800

    650 cpp

---
+++ 650. 2 Keys Keyboard/solution_1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int i = 2, step = 0;
+        while (n > 1) {
+            while (n % i == 0) {
+                step += i;
+                n = n / i;
+            }
+            i ++;
+        }
+        return step;
+    }
+};
