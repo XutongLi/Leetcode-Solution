f6fe3773975f52e4e7d8a405396e657d4430038e
XutongLi <brianleelxt@gmail.com>
2020-02-01 03:26:25 +0800

    add 387 cpp

---
+++ 387. First Unique Character in a String/soluton_1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> map(26, 0);
+        for (auto c : s) 
+            ++ map[c - 'a'];
+        for (int i = 0; i < s.size(); ++ i)
+            if (map[s[i] - 'a'] == 1)
+                return i;
+        return -1;
+    }
+};
