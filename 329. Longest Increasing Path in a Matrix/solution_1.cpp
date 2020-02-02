ccec7c2fdad7cf72478e0cd52a8ad0dcdf381c9e
XutongLi <brianleelxt@gmail.com>
2020-02-03 02:21:18 +0800

    add 329

---
+++ 329. Longest Increasing Path in a Matrix/solution_1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    void dfs(const vector<vector<int>> &arr, int i, int j, vector<vector<int>> &mem) {
+        mem[i][j] = 1;
+        for (int h = -1; h <= 1; ++ h) {
+            for (int v = -1; v <= 1; ++ v) {
+                if (h != 0 && v != 0 || h == 0 && v == 0)
+                    continue;
+                int x = i + h, y = j + v;
+                if (x < 0 || x >= arr.size() || y < 0 || y >= arr[0].size() || arr[x][y] <= arr[i][j])
+                    continue;
+                if (mem[x][y] == 0)
+                    dfs(arr, x, y, mem);
+                mem[i][j] = max(mem[i][j], mem[x][y] + 1);
+            }
+        }
+    }
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if (matrix.empty())
+            return 0;
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> mem(m, vector<int>(n, 0));  //mem为到m[i][j]结束的递增字串最大长度，未被访问时为0
+        int maxi = 0;
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                if (mem[i][j] > 0)
+                    continue;
+                dfs(matrix, i, j, mem);
+                maxi = max(maxi, mem[i][j]);
+            }
+        }
+        return maxi;
+    }
+};
