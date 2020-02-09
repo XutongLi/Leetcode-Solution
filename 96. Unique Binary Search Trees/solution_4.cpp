de2c195702b38f1b6dd7a72d7de1233aa4d4366b
XutongLi <brianleelxt@gmail.com>
2020-02-10 03:03:59 +0800

    add 96 cpp

---
+++ 96. Unique Binary Search Trees/solution_4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> vec(n + 1);
+        vec[0] = 1;
+        for (int i = 1; i <= n; ++ i) {
+            for (int j = 1; j <= i; ++ j)
+                vec[i] += vec[j - 1] * vec[i - j];
+        }
+        return vec[n];
+    }
+}; 
+//g(i) 表示序列长度为i的树个数
+//f(j, i) 表示序列长度为i， 以第j个节点作为根的树的个数
+//g(i) = sum_(j = 1)^(i)f(j, i)
+//f(j, i) = g(j - 1)*g(i - j)
+//所以 g(i) = sum_(j = 1)^(i)g(j - 1)*g(i - j)
