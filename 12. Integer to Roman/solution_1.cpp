5fc5ad652b4b01f3e869c2c698adbf519297663b
XutongLi <brianleelxt@gmail.com>
2020-02-15 03:46:31 +0800

    add 12 cpp

---
+++ 12. Integer to Roman/solution_1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    vector<pair<int, string>> mp = 
+    {
+        {1000, "M"}, {900, "CM"},
+        {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"},
+        {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"},
+        {5, "V"}, {4, "IV"}, {1, "I"}
+    };
+public:
+    string intToRoman(int num) {
+        string res = "";
+        for (auto p : mp) {
+            while (num >= p.first) {
+                res += p.second;
+                num -= p.first;
+            }
+        }
+        return res;
+    }
+};
