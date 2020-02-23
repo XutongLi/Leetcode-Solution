c141dd4d83c5cc4159156e22575d0205e5480df2
XutongLi <brianleelxt@gmail.com>
2020-02-24 03:52:57 +0800

    add 274 cpp

--- 274. H-Index/solution_1.cpp
+++ 274. H-Index/solution_1.cpp
@@ -4,12 +4,12 @@ public:
         if (citations.empty())
             return 0;
         sort(citations.rbegin(), citations.rend());
-        int n = citations.size(), i;
-        for (i = 0; i < n; ++ i) {
+        int n = citations.size();
+        for (int i = 0; i < n; ++ i) {
             if (citations[i] < i + 1)
-                break;
+                return i;
         }
-        return i;
+        return n;
     }
 };
 //O(nlogn)
