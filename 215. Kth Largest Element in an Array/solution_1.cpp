45200cbad8136e24f43ff8d1dc0b41eca1342279
XutongLi <brianleelxt@gmail.com>
2020-08-12 00:14:14 +0800

    update 215 cpp

--- 215. Kth Largest Element in an Array/solution_1.cpp
+++ 215. Kth Largest Element in an Array/solution_1.cpp
@@ -1,44 +1,42 @@
 class Solution {
-public:
-    int findKthLargest(vector<int>& input, int k) {
-        int len = input.size();
-        int lo = 0, hi = len - 1;
-        int idx = partition(input, lo, hi);
-        while (idx != k - 1) {
-            if (idx > k - 1) {
-                hi = idx - 1;
-                idx = partition(input, lo, hi);
-            }
-            else {
-                lo = idx + 1;
-                idx = partition(input, lo, hi);
-            }
-        }
-        return input[idx];
-    }
-    int partition(vector<int>& n, int lo, int hi) {
-        if (lo >= hi)
-            return lo;
-        int idx = RandomInRange(lo, hi);
-        swap(n[idx], n[hi]);
-        int large = lo - 1;
-        for (int i = lo; i < hi; i ++) {
-            if (n[i] > n[hi]) {
-                large ++;
-                swap(n[i], n[large]);
-            }
-        }
-        large ++;
-        swap(n[hi], n[large]);
-        return large;
-    }
-    int RandomInRange(int lo, int hi) {
-        srand(time(NULL));
+private:
+    int randInRange(int lo, int hi) {
+        srand(time(0));
         return lo + rand() % (hi - lo + 1);
     }
-    void swap(int& a, int& b) {
-        int t = a;
+    void swap(int &a, int &b) {
+        int tmp = a;
         a = b;
-        b = t;
+        b = tmp;
+    }
+    int partition(vector<int> &nums, int lo, int hi) {
+        int index = randInRange(lo, hi);
+        swap(nums[index], nums[hi]);
+        int small = lo - 1;
+        for (index = lo; index < hi; ++ index) {
+            if (nums[index] > nums[hi]) {
+                ++ small;
+                swap(nums[index], nums[small]);
+            }
+                
+        }
+        ++ small;
+        swap(nums[small], nums[hi]);
+        return small;
+    }
+    
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi) {
+            int pivot = partition(nums, lo, hi);
+            if (pivot + 1 < k) 
+                lo = pivot + 1;
+            else if (pivot + 1 > k)
+                hi = pivot - 1;
+            else
+                return nums[pivot];
+        }
+        return nums[lo];
     }
 };
