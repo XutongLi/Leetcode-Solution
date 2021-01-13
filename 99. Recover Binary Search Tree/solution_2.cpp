165a999d21d42eaf73dea25a117c5b1e885e655b
XutongLi <brianleelxt@gmail.com>
2021-01-13 18:48:02 +0800

    update 99

--- 99. Recover Binary Search Tree/solution_2.cpp
+++ 99. Recover Binary Search Tree/solution_2.cpp
@@ -4,34 +4,30 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 private:
-    void inorder(TreeNode *root, TreeNode *&pre, TreeNode *&small, TreeNode *&big) {
-        if (!root)
-            return;
-        inorder(root->left, pre, small, big);
-        if (pre && !big) {      //big取第一个当前节点大于小于上一个节点的情况（取上一个）
-            if (root->val < pre->val) 
-                big = pre;
-        }
-        if (pre) {      //small取最后一个当前节点小于上一个节点的情况（取当前）
-            if (root->val < pre->val) 
-                small = root;
+    void inorder(TreeNode *root, TreeNode *&pre, TreeNode *&big, TreeNode *&small) {
+        if (!root)	return;
+        inorder(root->left, pre, big, small);
+        if (pre && root->val < pre->val) {
+            small = root;
+            if (!big)		big = pre;
         }
         pre = root;
-        inorder(root->right, pre, small, big);
+        inorder(root->right, pre, big, small);
     }
 public:
     void recoverTree(TreeNode* root) {
-        TreeNode *pre = nullptr, *small = nullptr, *big = nullptr;  //small为两个交换的数字中正确情况下应该靠前的，big为应该靠后的
-        inorder(root, pre, small, big);
-        int val = small->val;
-        small->val = big->val;
-        big->val = val;
+        TreeNode *pre = nullptr, *big = nullptr, *small = nullptr;
+        inorder(root, pre, big, small);
+        swap(big->val, small->val);
     }
 };
+
 //inorder traversal dfs
 //time O(n)  space O(1)
