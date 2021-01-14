96e44b4412dfbb55ef66fd7324a4cb44f76f5401
XutongLi <brianleelxt@gmail.com>
2021-01-14 22:31:47 +0800

    update 257

--- 257. Binary Tree Paths/solution_1.cpp
+++ 257. Binary Tree Paths/solution_1.cpp
@@ -4,34 +4,27 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-    vector<string> res;
-public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        if (root == NULL)   return res;
-        string str = to_string(root->val);
-        if (root->left == NULL && root->right == NULL) {
-            res.push_back(str);
-            return res;
-        }
-        if (root->left)
-            addPath(root->left, str);
-        if (root->right)
-            addPath(root->right, str);
-        return res;
-    }
-    void addPath(TreeNode* root, string str) {
-        str += "->" + to_string(root->val);
-        if (root->left == NULL && root->right == NULL) {
-            res.push_back(str);
+private:
+    void dfs(TreeNode *root, string cur, vector<string> &res) {
+        cur += ("->" + to_string(root->val));
+        if (!root->left && !root->right) {
+            res.push_back(cur.substr(2));
             return;
         }
-        if (root->left)
-            addPath(root->left, str);
-        if (root->right)
-            addPath(root->right, str);
+        if (root->left) dfs(root->left, cur, res);
+        if (root->right)    dfs(root->right, cur, res);
+}
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        if (!root)  return res;
+        dfs(root, "", res);
+        return res; 
     }
 };
