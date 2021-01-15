aff54bc04a1f44f8c9f1226279446eeb4e3a6377
XutongLi <brianleelxt@gmail.com>
2021-01-16 03:11:53 +0800

    update 69

--- 69. Sqrt(x)/solution_2.cpp
+++ 69. Sqrt(x)/solution_2.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0)
-            return 0;
+        if (x == 0)	return 0;
         int l = 1, r = x;
-        while (l < r) {
-            int mid = (r - l) / 2 + l;
-            if (mid < x / mid && (mid + 1) > x / (mid + 1))
-                return mid;
-            else if (mid < x / mid) {
-                l = mid + 1;
-            }
+        while (l <= r) {
+            int m = (r - l) / 2 + l;
+            if (m <= x / m && (m + 1) > x / (m + 1))
+                return m;
+            else if (m > x / m)	
+                r = m - 1;
             else
-                r = mid;
+                l = m + 1;
         }
-        return l;
+        return -1;
     }
 };
-
-/*
-int mySqrt(int x) {
-        if (x == 0) return 0;
-        int n = 1;
-        while (true) {
-            if (n < x / n && (n + 1) > x / (n + 1))
-                break;
-            ++ n;
-        }
-        return n;
-    }
-*/
