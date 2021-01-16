618f42a745a7e019f0bb0a231a13479b9da902c4
XutongLi <brianleelxt@gmail.com>
2021-01-16 15:28:06 +0800

    update 35

--- 35. Search Insert Position/solution_2.cpp
+++ 35. Search Insert Position/solution_2.cpp
@@ -1,19 +1,17 @@
 class Solution {
+private:
+	int search(vector<int> &nums, int target, int l, int r) {
+		if (l > r)		return l;
+		int m = (r - l) / 2 + l;
+		if (nums[m] == target)	
+			return m;
+		else if (nums[m] > target)
+			return search(nums, target, l, m - 1);
+		else
+			return search(nums, target, m + 1, r);
+    }
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int len = nums.size();
-        return binarySearch(nums, target, 0, len - 1);
-    }
-    int binarySearch(vector<int> nums, int target, int lo, int hi) {
-        while (lo <= hi) {
-            int mid = (hi - lo) / 2 + lo;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] > target)
-                hi = mid - 1;
-            else
-                lo = mid + 1;
-        }
-        return lo;
+        return search(nums, target, 0, nums.size() - 1);
     }
 };
