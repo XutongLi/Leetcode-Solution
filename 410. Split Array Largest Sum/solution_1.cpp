bb4aa4a9a269aca447458367de2b976f52109361
XutongLi <brianleelxt@gmail.com>
2021-01-20 02:51:11 +0800

    update 410

--- 410. Split Array Largest Sum/solution_1.cpp
+++ 410. Split Array Largest Sum/solution_1.cpp
@@ -1,25 +1,28 @@
 class Solution {
 private:
-    void dfs(int idx, int m, vector<int> &nums, vector<vector<int>> &mem, int &res) {
+    void dfs(vector<int> &sums, vector<vector<int>> &mem, int m, int idx) {
         if (m == 1) {
-            mem[idx][m] = accumulate(nums.begin() + idx, nums.end(), 0);
+            mem[idx][m] = sums[sums.size() - 1] - sums[idx - 1];
             return;
         }
-        int sum = 0, cur_min_maxi = INT_MAX;
-        for (int i = idx; i <= nums.size() - m; ++ i) {
-            sum += nums[i];
+        int cur_min = INT_MAX;
+        for (int i = idx; i <= sums.size() - m; ++ i) {
+            int sum = sums[i] - sums[idx - 1];
             if (mem[i + 1][m - 1] == -1)
-                dfs(i + 1, m - 1, nums, mem, res);
-            cur_min_maxi = min(cur_min_maxi, max(sum, mem[i + 1][m - 1]));
+                dfs(sums, mem, m - 1, i + 1);
+            cur_min = min(cur_min, max(sum, mem[i + 1][m - 1]));
         }
-        mem[idx][m] = cur_min_maxi;
+        mem[idx][m] = cur_min;
     }
 public:
     int splitArray(vector<int>& nums, int m) {
-        int res = INT_MAX;
-        vector<vector<int>> mem(nums.size() + 1, vector<int>(m + 1, -1));
-        dfs(0, m, nums, mem, res);
-        return mem[0][m];
+        int len = nums.size();
+        vector<vector<int>> mem(len + 1, vector<int>(m + 1, -1));
+        vector<int> sums(len + 1, 0);
+        for (int i = 0; i < len; ++ i)
+            sums[i + 1] = sums[i] + nums[i];
+        dfs(sums, mem, m, 1);
+        return mem[1][m];
     }
 };
 //dfs with memoization(DP)
