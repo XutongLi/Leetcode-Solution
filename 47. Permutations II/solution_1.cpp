5c3c9d88315eaf6ea581571d60750815536197e5
XutongLi <brianleelxt@gmail.com>
2021-01-21 20:53:59 +0800

    update 44

--- 47. Permutations II/solution_1.cpp
+++ 47. Permutations II/solution_1.cpp
@@ -1,38 +1,33 @@
 class Solution {
 private:
-    void track(const vector<int> &nums, int idx, unordered_map<int, int> &mp, vector<int> &cur, vector<vector<int>> &res) {
-        if (idx == nums.size()) {
+    void dfs(vector<int>& nums, vector<vector<int>> &res, vector<int> &cur, unordered_map<int, int> &mp, int cnt) {
+        if (cnt == nums.size()) {
             res.push_back(cur);
             return;
         }
-        int pre = INT_MIN;
-        for (auto n : nums) {
-            if (mp[n] == 0)
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (mp[nums[i]] == 0)
                 continue;
-            if (pre == INT_MIN)     //初始化pre
-                pre = n;
-            else if (n == pre)      //下一个idx不放入相同数字
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            pre = n;
-            cur.push_back(n);
-            -- mp[n];
-            track(nums, idx + 1, mp, cur, res);
-            ++ mp[n];
+            cur.push_back(nums[i]);
+            -- mp[nums[i]];
+            dfs(nums, res, cur, mp, cnt + 1);
+            ++ mp[nums[i]];
             cur.pop_back();
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> res;
-        if (nums.empty())
-            return res;
-        sort(nums.begin(), nums.end());
-        unordered_map<int, int> mp;
-        for (auto n : nums)
-            ++ mp[n];
         vector<int> cur;
-        track(nums, 0, mp, cur, res);
+        unordered_map<int, int> mp;
+        for (int n : nums)
+            ++ mp[n];
+        sort(nums.begin(), nums.end());
+        dfs(nums, res, cur, mp, 0);
         return res;
     }
 };
+
 //backtracking
