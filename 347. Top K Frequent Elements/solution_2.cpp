77c91b53b4e39b83149439f975c6801a10b6a30a
XutongLi <brianleelxt@gmail.com>
2021-09-06 03:00:55 +0800

    update 347

--- 347. Top K Frequent Elements/solution_2.cpp
+++ 347. Top K Frequent Elements/solution_2.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
         unordered_map<int, int> mp;
+        int maxi = 0;
         for (auto n : nums) {
             ++ mp[n];
+            maxi = max(maxi, mp[n]);
         }
-        vector<vector<int>> m(nums.size() + 1);
-        vector<int> res;
-        for (auto p : mp) {
-            m[p.second].push_back(p.first);
-        }
-        for (int i = m.size() - 1; i > 0; -- i) {
-            for (auto n : m[i]) {
-                res.push_back(n);
+        vector<vector<int>> cnt(maxi + 1);
+        for (auto p : mp)
+            cnt[p.second].push_back(p.first);
+        for (int i = maxi; i > 0; -- i) {
+            for (auto idx : cnt[i]) {
+                res.push_back(idx);
                 if (res.size() == k)
                     return res;
             }
